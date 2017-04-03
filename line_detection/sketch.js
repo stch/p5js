1422c020dc62b86c417ae8d8b0170616d5b3b97a
Satoshi HOSHIBA <stch.goog@gmail.com>
2017-04-03 17:07:36 +0900

    とりあえずD&Dでファイル読み込み対応

--- line_detection/sketch.js
+++ line_detection/sketch.js
@@ -1,8 +1,35 @@
+
+var img
+
 function setup() {
-  hello = "hello world!"
+  // create canvas
+  var c = createCanvas(screen.width, screen.height);
+  background(100);
+  fill(255);
+  noStroke();
+  textSize(24);
+  textAlign(CENTER);
+  text('Drag an image file onto the canvas.', width/2, height/2);
+
+  // Add an event for when a file is dropped onto the canvas
+  c.drop(gotFile);
 }
 
 function draw() {
   clear()
-  text(hello,10,10)
+  // Draw the image onto the canvas
+  if(img){
+    image(img, 0, 0, width/2, 0.5*width*img.height/img.width);
+  }
+
+}
+
+function gotFile(file) {
+  // If it's an image file
+  if (file.type === 'image') {
+    // Create an image DOM element but don't show it
+    img = createImg(file.data).hide();
+  } else {
+    println('Not an image file!');
+  }
 }
